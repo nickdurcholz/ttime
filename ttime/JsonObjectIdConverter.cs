06884a6605ae6fb7016b5a0e02e820713f423444
Nick Durcholz <nick.durcholz@pm.me>
2019-02-01 10:46:12 -0500

    Serialize ObjectId's as strings in json

---
+++ ttime/JsonObjectIdConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LiteDB;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace ttime
+{
+    public class JsonObjectIdConverter : JsonConverter<ObjectId>
+    {
+        public override void WriteJson(JsonWriter writer, ObjectId value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString());
+        }
+
+        public override ObjectId ReadJson(
+            JsonReader reader,
+            Type objectType,
+            ObjectId existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.String)
+            {
+                var jsonLineInfo = (IJsonLineInfo)reader;
+                throw new JsonSerializationException(
+                    $"Unexpected token parsing ObjectId. Expected String, got {reader.TokenType}.",
+                    reader.Path,
+                    jsonLineInfo.LineNumber,
+                    jsonLineInfo.LinePosition,
+                    null);
+            }
+
+            var sval = reader.Value?.ToString();
+            return string.IsNullOrEmpty(sval) ? null : new ObjectId(sval);
+        }
+    }
+}
--- ttime/TimeEntry.cs
+++ ttime/TimeEntry.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using LiteDB;
+using Newtonsoft.Json;
 
 namespace ttime
 {
     public class TimeEntry
     {
+        [JsonConverter(typeof(JsonObjectIdConverter))]
         public ObjectId Id { get; set; }
         public DateTime Time { get; set; }
         public bool Stopped { get; set; }
